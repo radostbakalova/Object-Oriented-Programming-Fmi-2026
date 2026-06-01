0ab3f808fa0afff2275b3fadbe5cc69f17dfb83e
Serhan Chavdarliev <67830778+darimachine@users.noreply.github.com>
2026-06-02 00:37:24 +0300

    Update URLBuilder_after.cpp

--- Group 6/Week 16/Seminar 16/Builder/URLBuilder_after.cpp
+++ Group 6/Week 16/Seminar 16/Builder/URLBuilder_after.cpp
@@ -76,7 +76,7 @@ public:
 int main() {
     URL url = URL::builder("https", "www", "example.com")
         .setPath("/index.html")
-        .setCountry("BGN")
+        .setCountry("BG")
         .build();
     std::cout << url.toString() << std::endl;
     return 0;
